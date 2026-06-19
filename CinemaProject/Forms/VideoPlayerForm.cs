8732c86f8cf9c3dcee2d96c02ea252184522651b
DRACKDOGG <kirillh07@gmail.com>
2026-06-19 20:17:51 +0300

    Добавьте файлы проекта.

---
+++ CinemaProject/Forms/VideoPlayerForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+using CinemaProject.Models;
+using CinemaProject.Services; // Обертка плеера лежит здесь по структуре проекта
+
+namespace CinemaProject.Forms
+{
+    public partial class VideoPlayerForm : Form
+    {
+        private readonly Movie movie_;
+        private readonly string quality_;
+        private readonly MediaPlayer mediaPlayer = new MediaPlayer();
+        private bool isPlaying = true;
+
+        public VideoPlayerForm(Movie movie, string quality)
+        {
+            InitializeComponent();
+            movie_ = movie;
+            quality_ = quality;
+        }
+
+        private void VideoPlayerForm_Load(object sender, EventArgs e)
+        {
+            string path = movie_.GetVideoPathByQuality(quality_);
+            mediaPlayer.LoadMovie(path);
+
+            mediaPlayer.PositionChanged += (sec) =>
+            {
+                if (tbSeek.IsDisposed) return;
+                tbSeek.Value = Math.Min(sec, tbSeek.Maximum);
+                lblStatus.Text = "Файл: " + path + " | Время: " + mediaPlayer.GetCurrentPosition().ToString("mm\\:ss");
+            };
+            mediaPlayer.Play();
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            if (isPlaying) { mediaPlayer.Pause(); btnPlayPause.Text = "Старт"; }
+            else { mediaPlayer.Play(); btnPlayPause.Text = "Пауза"; }
+            isPlaying = !isPlaying;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Stop();
+            this.Close();
+        }
+
+        private void tbVolume_Scroll(object sender, EventArgs e) { mediaPlayer.SetVolume(tbVolume.Value); }
+        private void tbSeek_Scroll(object sender, EventArgs e) { mediaPlayer.Seek(tbSeek.Value); }
+    }
+}
---
+++ CinemaProject/Services/UserManager.cs
@@ -0,0 +1,49 @@
+﻿using CinemaProject.Models;
+using CinemaProject.Services;
+using CinemaProject.Repositories;
+using System.Collections.Generic;
+
+namespace CinemaProject.Managers
+{
+    public class UserManager
+    {
+        private List<User> users_ = new List<User>();
+        private User currentUser_ = new User();
+        private readonly UserRepository repo_ = new UserRepository();
+
+        public User CurrentUser
+        {
+            get { return currentUser_; }
+        }
+
+        public bool Register(string login, string password)
+        {
+            if (repo_.IsLoginExists(login)) return false;
+            repo_.RegisterUser(login, password, "User");
+            return true;
+        }
+
+        public User Authenticate(string login, string password)
+        {
+            User user = repo_.Authenticate(login, password);
+            if (user != null)
+            {
+                user.History = repo_.GetUserHistory(user.Id);
+                currentUser_ = user;
+                return currentUser_;
+            }
+            return null;
+        }
+
+        public void AddToHistory(int movieId)
+        {
+            int finalUserId = (currentUser_ != null && currentUser_.Id > 0) ? currentUser_.Id : 1;
+            repo_.AddToHistory(finalUserId, movieId);
+        }
+
+        public List<Movie> GetPersonalRecommendations(MovieManager movieManager)
+        {
+            return repo_.GetPersonalRecommendations(currentUser_.Id);
+        }
+    }
+}
